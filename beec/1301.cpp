74df305faee977d527aea1f94aabc0361ea21891
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2024-05-04 19:34:19 -0300

    1031

--- beec/1301.cpp
+++ beec/1301.cpp
@@ -9,27 +9,30 @@ int main() {
     int k; // numero de comandos apos a entrada 
     int aux; 
 
-    vector<int> vector; // vetor de valores 
+    int* vector; // vetor de valores 
     char opt; // qual o comando a ser executado no vetor 
     int prod; // produto dos elementos especificados 
     int pos_i, pos_f; // posição dos elementos para multiplicar
 
     int pos_mod, val_mod; // posição e o valor a serem modifcados 
 
-    while (!cin.eof()) {
-        cin >> n;
-        cin >> k; 
+    while (cin >> n >> k) {
+        // cin >> n;
+        // cin >> k; 
+        // cout << "N: " << n << "K: " << k; 
+
+        vector = (int*) malloc(sizeof(int)*n);
 
         // coletar valores do vetor
         for (int i = 0; i < n; i++) {
             cin >> aux;
-            vector.push_back(aux);
+            vector[i] = aux;
         }
 
         // iniciar operações 
         for (int i = 0 ; i < k; i++) {
             cin >> opt; 
-             if (opt == 'C') {
+            if (opt == 'C') {
                 cin >> pos_mod; 
                 cin >> val_mod;
                 vector[pos_mod - 1] = val_mod;
@@ -40,15 +43,21 @@ int main() {
                 prod = 1; 
 
                 for (int j = pos_i-1; j <= pos_f-1; j++) {
-                    prod *= vector[j]; 
+                    if (vector[j] == 0) {
+                        prod = 0;
+                        break;
+                    }
+                    if (vector[j] < 0) prod = -prod; 
                 } 
 
                 if (prod < 0) cout << "-";
                 else if (prod == 0) cout << "0";
                 else cout << "+";
             }
+            // printVector(vector, n);
         }
         cout << endl; 
+        free(vector); 
     }
 
     return 0;
--- beec/testes.cpp
+++ beec/testes.cpp
@@ -9,3 +9,10 @@ int main() {
 
     return 0;
 }
+
+
+int printVector(int* vector , int size) {
+    std::cout << std::endl; 
+    for (int i = 0; i < size; i++) std::cout << " " << vector[i]; 
+    std::cout << std::endl; 
+}
