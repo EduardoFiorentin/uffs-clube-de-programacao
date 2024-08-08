f7934d468154b14e4a06497ce9aead0496fc04fb
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2024-08-08 12:26:58 -0300

    tentativa fracassada de otimização desta porcaria

--- cses/concertTickets.cpp
+++ cses/concertTickets.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 using namespace std; 
 int main() {
-    long n, m, ref, index;
+    long n, m, ref;
     cin >> n >> m;
     vector<long> tickets, costumers; 
-
+ 
     for (int i = 0; i < n; i++) {
         cin >> ref; 
         tickets.push_back(ref);
@@ -14,18 +14,18 @@ int main() {
     sort(tickets.begin(), tickets.end());
     
     for (int i = 0; i < m; i++) {
-
+ 
         cin >> ref;
-
+ 
         vector<long>::iterator it = upper_bound(tickets.begin(), tickets.end(), ref);
         
         if (it == tickets.begin()) {
             cout << -1 << endl; 
-        } 
-        else {
-            index = distance(tickets.begin(), it);
-            cout << tickets.at(index - 1) << endl; 
-            tickets.erase(tickets.begin() + (index - 1));
+ 
+        } else {
+            it--; 
+            cout << *it << endl; 
+            tickets.erase(it);
         }
     }   
 }
